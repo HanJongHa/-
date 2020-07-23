e91c6f17af76d567317657f556745aa8e55acf8b
ywooKim <yeong8211@gmail.com>
2020-07-23 17:25:49 +0900

    로그인 페이지(모형), 지도 뷰 초기 구현 Android Studio 파일

---
+++ TESTRightnow/app/src/main/java/com/example/testrightnow/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.testrightnow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.naver.maps.map.NaverMapSdk;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button loginbutton = findViewById(R.id.loginbutton);
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MapActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
