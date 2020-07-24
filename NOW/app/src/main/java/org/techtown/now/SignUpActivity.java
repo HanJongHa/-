a567d74a31fdbb473245966bc676eec44ecb3cdd
한종하 <gkswhdgk81@naver.com>
2020-07-24 21:49:03 +0900

    회원가입 데이터베이스 연결 추가 commit(이거 커밋해서 이상생기면 이전 커밋으로 돌려버려도돼)

--- NOW/app/src/main/java/org/techtown/now/SignUpActivity.java
+++ NOW/app/src/main/java/org/techtown/now/SignUpActivity.java
@@ -7,23 +7,156 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class SignUpActivity extends AppCompatActivity {
+    private static String IP_ADDRESS = "27.96.134.241";
+    private static String TAG = "phptest";
 
+    private EditText mEditTextuser_id;
+    private EditText mEditTextuser_pw;
+    private TextView mTextViewResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        mEditTextuser_id = (EditText)findViewById(R.id.editText2);
+        mEditTextuser_pw = (EditText)findViewById(R.id.editText3);
+        mTextViewResult = (TextView)findViewById(R.id.textView_main_result);
+
+        mTextViewResult.setMovementMethod(new ScrollingMovementMethod());
+
+        Button buttonInsert = (Button)findViewById(R.id.button);
+        buttonInsert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
-                setResult(RESULT_OK,intent);
-                finish();
-                Toast.makeText(getApplicationContext(), "회원가입이 완료 되었습니다!", Toast.LENGTH_LONG).show();
+
+                String u_id = mEditTextuser_id.getText().toString();
+                String u_pw = mEditTextuser_pw.getText().toString();
+
+                InsertData task = new InsertData();
+                task.execute("http://" + IP_ADDRESS + "/htdocs/insert3.php", u_id,u_pw);
+
+
+                mEditTextuser_id.setText("");
+                mEditTextuser_id.setText("");
+
             }
         });
     }
+
+    class InsertData extends AsyncTask<String, Void, String>{
+        ProgressDialog progressDialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = ProgressDialog.show(SignUpActivity.this,
+                    "Please Wait", null, true, true);
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            progressDialog.dismiss();
+            mTextViewResult.setText(result);
+            Log.d(TAG, "POST response  - " + result);
+        }
+
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String u_id = (String)params[1];
+            String u_pw = (String)params[2];
+
+            String serverURL = (String)params[0];
+            String postParameters = "u_id=" + u_id + "&u_pw=" + u_pw;
+
+
+            try {
+
+                URL url = new URL(serverURL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.connect();
+
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes("UTF-8"));
+                outputStream.flush();
+                outputStream.close();
+
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d(TAG, "POST response code - " + responseStatusCode);
+
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                }
+                else{
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+
+
+                bufferedReader.close();
+
+
+                return sb.toString();
+
+
+            } catch (Exception e) {
+
+                Log.d(TAG, "InsertData: Error ", e);
+
+                return new String("Error: " + e.getMessage());
+            }
+
+        }
+    }
+
 }
