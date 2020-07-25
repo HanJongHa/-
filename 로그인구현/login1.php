386d1560981f3a18ac8ce9ef651bfbdb6e064cc3
한종하 <gkswhdgk81@naver.com>
2020-07-25 17:04:21 +0900

    앱에서 로그인 구현하는 php파일 및 안드로이드에 추가해야할 사항 적어둠 근데 아직 로그인 구현은 못함 그대로했는데 안되누

--- 로그인구현/login1.php
+++ 로그인구현/login1.php
@@ -1,24 +1,22 @@
-<?php 
-     error_reporting(E_ALL); 
-     ini_set('display_errors',1); 
- 
-     include('dbcon.php'); // db에 연결을 하는코드
-    //$con = myqli_cnnect("db-4ha7m.cdb.ntruss.com","sweetboys","whdgk123!","main_db");
-    //mysqli_query($con,'SET NAMES utf8');
+<?php
+    error_reporting(E_ALL); 
+    ini_set('display_errors',1); 
 
-    $u_id =$_POST["u_id"];
-    $u_pw =$_POST["u_pw"];
+    include('dbcon.php'); // db에 연결을 하는코드
+    //$con = mysqlli_connect('db-4ha7m.cdb.ntruss.com','sweetboys','whdgk123!','main_db');
+
+    $u_id = $_POST["u_id"];
+    $u_pw = $_POST["u_pw"];
 
     $statement = mysqli_prepare($con,"select * from custom_info where u_id=? and u_pw=?");
     mysqli_stmt_bind_param($statement,"ss",$u_id,$u_pw);
     mysqli_stmt_execute($statement);
-
-
     mysqli_stmt_store_result($statement);
     mysqli_stmt_bind_result($statement,$u_id,$u_pw);
 
     $response = array();
-    $response["success"]=true;
+    $response["success"]=false;
+
     while(mysqli_stmt_fetch($statement)){
         $response["success"]=true;
         $response["u_id"]=$u_id;
